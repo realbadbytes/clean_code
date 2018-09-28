c042f57b4fa543adc8b74983cd372cbaa040e033
realbadbytes <badbytes.io@gmail.com>
2018-09-28 15:07:25 -0700

    use good var names, idiots

---
+++ pronounceable_words.java
@@ -0,0 +1,18 @@
+/* shit code
+new dev would have to interpret this shit */
+
+class DtaRcrd102 {
+    private Date genymdhms;
+    private Date modymdhms;
+    private final String pszqint = "102";
+};
+
+/* less shit code
+maintainable 
+able to discuss code in review without sounding retarded */
+
+class Customer {
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+}
