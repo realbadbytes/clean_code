70db96645e5db363546af44a2ededbfef9983085
realbadbytes <badbytes.io@gmail.com>
2018-10-17 20:09:16 -0700

    horizontal alignment, but i dont agree

---
+++ formatting/horizontal_alignment.java
@@ -0,0 +1,12 @@
+/* dont align things like this because it distracts a reader from grokking/viewing the type */
+
+private Socket      socket;
+private InputStream input;
+private long        flags;
+
+/* I disagree with this particular style guidance and much prefer the former to the latter */
+
+private Socket socket;
+private InputStream input;
+private long flags;
+
