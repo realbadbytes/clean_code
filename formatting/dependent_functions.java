fb7418c3f87438d32bae284ba98fc29c6eb8f367
realbadbytes <badbytes.io@gmail.com>
2018-10-17 19:49:33 -0700

    dependent functions in class

---
+++ formatting/dependent_functions.java
@@ -0,0 +1,50 @@
+/* if one function calls another, those functions should be physically close
+ * topmost function calls functions defined below it in a class
+ *
+ * makeResponse() -> getPageNameOrDefault()
+ *                -> notFoundResponse()
+ *                -> makePageResponse()
+ *
+ * its also great practice to have them in order of calling! although that 
+ * is not necessary unless you are doing a very meticulous code cleanup/style effort.
+ */
+
+public class WikiPageResponder implements SecureResponder {
+    protected WikiPage page;
+    protected PageData pageData;
+    protected String pageTitle;
+    protected Request request;
+    protected PageCrawler crawler;
+
+    public Response makeResponse(FitnesseContext context, Request request) throws Exception {
+        String pageName = getPageNameOrDefault(request, "FrontPage");
+        loadPage(pageName, context);
+        if (page == null)
+            return notFoundResponse(context, request);
+        else
+            return makePageResponse(context);
+    }
+
+    private String getPageNameOrDefault(Request request, String defaultPageName) throws Exception {
+        WikiPagePath path = PathParser.parse(resource);
+        crawler = context.root.getPageCrawler();
+        crawler.setDeadEndStrategy(new VirtualEnabledPageCrawler());
+        page = crawler.getPage(context.root, path);
+        if (page != null)
+            pageData = page.getData();
+    }
+
+    private Response notFoundResponse(FitnesseContext context, Request request) throws Exception {
+        return new NotFoundResponder().makeResponse(context, request);
+    }
+
+    private SimpleResponse makePageResponse(FitnesseContext context) throws Exception {
+        pageTitle = PathParser.render(crawler.getFullPath(page));
+        String html = makeHtml(context);
+
+        SimpleResponse response = new SimpleResponse();
+        response.setMaxAge(0);
+        response.setContent(html);
+        return response;
+    }
+}
